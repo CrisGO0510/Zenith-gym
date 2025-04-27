7d5db8705ec6412d4338f0b94da9fe06199ec039
CrisGO <cristhiangiraldo053@gmail.com>
2025-04-27 12:38:30 -0500

    Modulo de login terminado

--- app/src/app/login/login.component.ts
+++ app/src/app/login/login.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface LoginForm {
+  username: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
   standalone: false,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  hidePassword: WritableSignal<boolean> = signal(true);
+  loginForm: FormGroup;
 
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  navigateTo(path: string) {
+    console.log('Navigating to:', path);
+    this.router.navigate([path]);
+  }
+
+  submitForm() {
+    if (this.loginForm.valid) {
+      const formValue: LoginForm = this.loginForm.value;
+      console.log('Form submitted', formValue);
+    } else {
+      this.markFormGroupTouched(this.loginForm);
+    }
+  }
+
+  private markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach((control) => {
+      control.markAsTouched();
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      }
+    });
+  }
 }
--- app/src/app/login/login.module.ts
+++ app/src/app/login/login.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
-  ]
+    LoginRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
