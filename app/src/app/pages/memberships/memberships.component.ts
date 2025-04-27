247d7537de99b7cd80b0bb31cbc409afbf305fbe
CrisGO <cristhiangiraldo053@gmail.com>
2025-04-27 09:12:54 -0500

    Estructura de front, estilos y rutas, implementadas

---
+++ app/src/app/pages/memberships/memberships.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-memberships',
+  standalone: false,
+  templateUrl: './memberships.component.html',
+  styleUrl: './memberships.component.scss'
+})
+export class MembershipsComponent {
+
+}
---
+++ app/src/app/pages/memberships/memberships.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MembershipsComponent } from './memberships.component';
+
+
+
+@NgModule({
+  declarations: [
+    MembershipsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MembershipsModule { }
