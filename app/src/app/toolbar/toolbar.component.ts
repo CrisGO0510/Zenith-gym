0adf67f2094e3a00af51726f28ffd1fb55e6c06b
CrisGO <cristhiangiraldo053@gmail.com>
2025-04-27 22:25:04 -0500

    Modulo de clientes finalizado

--- app/src/app/toolbar/toolbar.component.ts
+++ app/src/app/toolbar/toolbar.component.ts
@@ -1,5 +1,8 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { DialogEditUserComponent } from '../core/components/dialog-edit-user/dialog-edit-user.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,15 +10,40 @@ import { Router } from '@angular/router';
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
 })
-export class ToolbarComponent {
-  constructor(private router: Router) {}
+export class ToolbarComponent implements OnDestroy {
+  private subscription$: Subscription = new Subscription();
+
+  constructor(
+    private router: Router,
+    private dialog: MatDialog,
+  ) {}
 
   navigateTo(path: string) {
     console.log('Navigating to:', path);
     this.router.navigate([path]);
   }
 
+  editUser() {
+    const dialogRef = this.dialog.open(DialogEditUserComponent, {
+      width: '80%',
+    });
+
+    this.subscription$.add(
+      dialogRef.afterClosed().subscribe((result) => {
+        if (result) {
+          console.log('User edited:', result);
+        } else {
+          console.log('Edit user dialog closed without changes');
+        }
+      }),
+    );
+  }
+
   logout() {
     console.log('Logging out...');
+  }
+
+  ngOnDestroy() {
+    this.subscription$.unsubscribe();
   }
 }
