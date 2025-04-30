a01ffc67059b533a0e6a64a165f2bca8325f50f6
CrisGO <cristhiangiraldo053@gmail.com>
2025-04-30 00:54:14 -0500

    Estructura de modulo de administrative terminada

--- app/src/app/core/interfaces/user.interface.ts
+++ app/src/app/core/interfaces/user.interface.ts
@@ -2,9 +2,9 @@ export interface User {
   userId: number;
   name: string;
   lastname: string;
-  email: string;
-  password: string;
-  phone: string;
+  email?: string;
+  password?: string;
+  phone?: string;
   birthdate: Date;
   restriction?: string;
 }
