6466a827988d9c58a54d617dabaa5baafbfede0f
CrisGO <cristhiangiraldo053@gmail.com>
2025-04-29 20:52:01 -0500

    Formulario integrado a dialog-view-reservation

--- app/src/app/core/components/dialog-view-reservation/dialog-view-reservation.component.ts
+++ app/src/app/core/components/dialog-view-reservation/dialog-view-reservation.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialogModule,
@@ -10,10 +10,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
-interface DialogViewData {
-  reservation: Reservation;
-}
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { SendDataDialog } from '../../interfaces/send-data-dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { ReservationStatus } from '../../interfaces/reservation-status.enum';
 
 @Component({
   selector: 'app-dialog-view-reservation',
@@ -25,20 +25,50 @@ interface DialogViewData {
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-    DatePipe,
+    ReactiveFormsModule,
+    MatSelectModule,
   ],
   templateUrl: './dialog-view-reservation.component.html',
   styleUrls: ['./dialog-view-reservation.component.scss'],
 })
-export class DialogViewReservationComponent {
-  reservationData!: Reservation;
+export class DialogViewReservationComponent implements OnInit {
+  reservationForm: FormGroup = new FormGroup({});
+  reservationStatusOptions: { value: ReservationStatus; label: string }[] = [];
 
   constructor(
     public dialogRef: MatDialogRef<DialogViewReservationComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogViewData,
-  ) {
-    console.log('Datos recibidos en diálogo:', this.data.reservation);
-    this.reservationData = data.reservation;
+    @Inject(MAT_DIALOG_DATA) public data: SendDataDialog<Partial<Reservation>>,
+    private formBuilder: FormBuilder,
+  ) {}
+
+  protected buildForm(item: Partial<Reservation> = {}): FormGroup {
+    return this.formBuilder.group({
+      id: item.id,
+      clientId: item.clientId,
+      clientName: item.clientName,
+      clientLastName: item.clientLastName,
+      routineId: item.routineId,
+      routineName: item.routineName,
+      instructorId: item.instructorId,
+      instructorName: item.instructorName,
+      instructorLastName: item.instructorLastName,
+      routineDescription: item.routineDescription,
+      startDate: item.startDate,
+      endDate: item.endDate,
+      reservationStatusId: item.reservationStatusId ?? 2,
+      reservationStatus: item.reservationStatus,
+    });
+  }
+
+  ngOnInit(): void {
+    console.log('Datos recibidos en diálogo:', this.data);
+    this.reservationStatusOptions = Object.values(ReservationStatus)
+      .filter((value): value is ReservationStatus => typeof value === 'number')
+      .map(status => ({
+        value: status,
+        label: ReservationStatus.toLabel(status)
+      }));
+    this.reservationForm = this.buildForm(this.data.data);
   }
 
   closeDialog(): void {
